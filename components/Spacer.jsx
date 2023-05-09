d08d9cd0cf67d9f5f93c74acddc428c1e640deac
Raghavendra Singh <raghavendrasingh989@gmail.com>
2023-05-09 14:38:39 +0530

    Updated spacing for hero section

--- components/Spacer.jsx
+++ components/Spacer.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 const Spacer = () => {
-  return (
-    <div className="bg-ghostWhite h-24 sm:h-12 lg:hidden w-full"></div>
-  );
+  return <div className="bg-ghostWhite h-16 lg:hidden w-full"></div>;
 };
 
 export default Spacer;
